7f97cb9aaff5f43c1a114649b0ba397a79f60689
ZZZaratustra <varlamcheg@inbox.ru>
2022-11-20 05:44:38 +0300

    second commit
    catch errors and responce json data

--- services/api.service.js
+++ services/api.service.js
@@ -1,9 +1,26 @@
-import https from 'https'
+import axios from 'axios'
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
 
 const getWeather = async (city) => {
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-
-    https.get()
+const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token)
+    if (!token) {
+        throw new Error('Не задан ключ API, задай его через -t [API_KEY]')
+    }
+    
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'ru',
+            units: 'metric'
+        }
+    })
+    
+    console.log(data)
+    return data
 }
 
-//06/07
+
+
+export { getWeather }
+
--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 #!/user/bin/env node
 import { getArgs } from './helpers/args.js'
+import { getWeather } from './services/api.service.js'
 import { printHelp, printError, printSuccess } from './services/log.service.js'
-import { saveKeyValue } from './services/storage.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -9,15 +10,32 @@ const saveToken = async (token) => {
         return
     }
     try{
-        await saveKeyValue('token', token)
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
         printSuccess('Токен сохранён')
     } catch (e) {
         printError('Токен не сохранён', e.message)
     }
+
 }
+const getForcast = async () => {
+    try {
+        const weather = await getWeather('Санкт-Петербург', '89fc46fa491b609d5b0ecaf99169c45f')
+        console.log(weather)
+    } catch (env) {
+        if (e?.responce?.status == 404) {
+            printError('Неверно указан город')
+        } else if (e?.responce?.status == 401) {
+            printError('Неверно указан ТОКЕН')
+        } else {
+            printError('e.message')
+        }
+    }
+}
+
 
 const initCLI = () => {
     const args = getArgs(process.argv)
+    // console.log(process.env)
         if (args.h){
             printHelp()         //вызвать помощь
         }
@@ -28,6 +46,11 @@ const initCLI = () => {
             return saveToken(args.t) //сохранить токен
         }
         //вывести погоду
+        getForcast()
 }
+// getWeather('Санкт-Петербург', '89fc46fa491b609d5b0ecaf99169c45f')
+// getWeather('Уфа', '89fc46fa491b609d5b0ecaf99169c45f')
+// getWeather('Пушкин', '89fc46fa491b609d5b0ecaf99169c45f')
+
 
 initCLI()
